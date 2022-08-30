d20d85fa7025ba33c2ca583a1c33ad9880c09248
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2022-08-30 10:08:42 -0400

    created function Person + mary as a sample Person

--- index.js
+++ index.js
@@ -15,9 +15,32 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(someFood) {
+  if(this.stomach.length < 10) {
+    this.stomach.push(someFood);
+  }
+} 
+
+Person.prototype.poop = function() {
+  this.stomach = [];
+}
+
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`;
+}
+
+const mary = new Person('Mary', 50);
+console.log(mary);
+mary.eat("pizza");
+console.log(mary.stomach);
+console.log(mary.toString());
+mary.poop();
 
 
 /*
